cd15ec4ead133898221ea4df6d094a3176e65fdd
kavitha <kavitha.srimanthappa@gmail.com>
2021-09-08 09:44:15 +0200

    added new file

---
+++ src/test/java/Allaince/Propertylines/SeleniumTest1.java
@@ -0,0 +1,13 @@
+package Allaince.Propertylines;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest1 {
+	
+	@Test	
+	public void myFirstTest1()
+	{
+		System.out.println("this is my first maven project");
+	}
+
+}
